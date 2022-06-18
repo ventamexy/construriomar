2981d326c7e431ba4681a75379364bcea7e9fed0
RamonMC <ramcruz1993@gmail.com>
2022-06-18 14:08:55 -0500

    Cambios generales.

---
+++ js/swiper.js
@@ -0,0 +1,51 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    effect: 'coverflow',
+    autoplay: {
+        delay: 5000,
+    },
+});
+
+const swiperLlave = new Swiper('.swiper-llave', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  autoplay: {
+      delay: 5000,
+  },
+});
